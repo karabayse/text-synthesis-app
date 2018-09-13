1631fea6f9f375963a3f4ec756d0472fa23b6994
Kara Bayse <karabayse@gmail.com>
2018-09-13 13:30:42 -0500

    initialized API and added variables for DOM elements

---
+++ client.js
@@ -0,0 +1,11 @@
+// Initialize SpeechSynth API
+const synth = window.speechSynthesis;
+
+// Grab DOM elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
